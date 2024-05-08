9757a2caf9baadb0a32436cec19210cca18a8d8a
Dinh Le <lqdquangdinh@gmail.com>
2024-05-08 11:35:13 +0300

    dummy list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
+
+import PeopleList from './components/PeopleList';
 
 function App() {
-  return (
-    <Container>
-      <Button variant="outlined">Hello button</Button>
-    </Container>
-  );
+  return <PeopleList />;
 }
 
 export default App;
