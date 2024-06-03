bbf9031ec5ba5d5f9a448c384055d37c933dadf4
Dinh Le <lqdquangdinh@gmail.com>
2024-06-03 10:09:41 +0300

    half of players context

--- src/store/players-context.tsx
+++ src/store/players-context.tsx
@@ -1,5 +1,44 @@
 import React, { createContext, useState, useRef, useEffect } from 'react';
+import { Player } from '../models/player.model';
 
 type PlayersContextObj = {
-  players: string[];
+  players: Player[];
+  addPlayer: (player: Player) => void;
+  removePlayer: (player: Player) => void;
 };
+
+export const PlayersContext = createContext<PlayersContextObj>({
+  players: [],
+  addPlayer: () => {},
+  removePlayer: () => {},
+});
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const PlayersContextProvider = ({ children }: Props) => {
+  const [players, setPlayers] = useState<Player[]>([]);
+
+  const addPlayer = (player: Player) => {
+    setPlayers((prevPlayers) => [...prevPlayers, player]);
+  };
+
+  const removePlayer = (player: Player) => {
+    setPlayers((prevPlayers) => prevPlayers.filter((p) => p.id !== player.id));
+  };
+
+  const contextValue: PlayersContextObj = {
+    players: players,
+    addPlayer: addPlayer,
+    removePlayer: removePlayer,
+  };
+
+  return (
+    <PlayersContextProvider.Provider value={contextValue}>
+      {children}
+    </PlayersContextProvider.Provider>
+  );
+};
+
+export default PlayersContextProvider;
